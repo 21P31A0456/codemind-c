7050ade80d6f5e089eea3429117c085c1e46690c
21P31A0456 <98729677+21P31A0456@users.noreply.github.com>
2022-02-23 11:40:03 +0530

    #include<stdio.h> int main() {     char c;     int lowercase_vowel,uppercase_vowel;     scanf("%c",&c);     lowercase_vowel=(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');     uppercase_vowel=(c=='A'||c=='E'||c=='I'||c=='O'||c=='U');     if(lowercase_vowel||uppercase_vowel)     printf("Vowel");     else     printf("Consonant");
    }

---
+++ Program_to_check_Vowel_or_Consonant.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    int lowercase_vowel,uppercase_vowel;
+    scanf("%c",&c);
+    lowercase_vowel=(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
+    uppercase_vowel=(c=='A'||c=='E'||c=='I'||c=='O'||c=='U');
+    if(lowercase_vowel||uppercase_vowel)
+    printf("Vowel");
+    else
+    printf("Consonant");
+    
+}
