264e2a16fd2e9996074118467dfbfb38f010a894
21P31A0456 <98729677+21P31A0456@users.noreply.github.com>
2022-04-19 14:41:50 +0530

    #include <stdio.h> int main() {     double fact;     int k,i,m,n;     scanf("%d", &n);     for (i=n; i>0; i--)     {         fact=1;         scanf("%d", &m);         for(k=2; k<=m; k++)             fact *= k;         printf("%.0lf ", fact);     }     return 0; }

---
+++ Small_Factorials.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    double fact;
+    int k,i,m,n;
+    scanf("%d", &n);
+    for (i=n; i>0; i--)
+    {
+        fact=1;
+        scanf("%d", &m);
+        for(k=2; k<=m; k++)
+            fact *= k;
+        printf("%.0lf
+", fact);
+    }
+    return 0;
+}
+
+
